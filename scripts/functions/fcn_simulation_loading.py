7834884abe663215d6da794d531e4bd2f2db1fc6
Lia Papadopoulos <liap@uoregon.edu>
2025-01-14 11:23:47 -0800

    adding function for setting up parameter sweep

---
+++ scripts/functions/fcn_simulation_loading.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+functions for loading simulations
+"""
+
+
+def fcn_set_sweepParam_string(n_sweepParams, sweep_param_name, swept_params_dict, param_indx):
+    
+    param_var_name = 'param_vals1'
+    param_val = swept_params_dict[param_var_name][param_indx]
+    sweep_param_str = ( '%s%0.3f' % (sweep_param_name, param_val) )
+    
+    for i in range(1, n_sweepParams):
+    
+        param_var_name = ('param_vals%d' % (i+1))
+        param_val = swept_params_dict[param_var_name][param_indx]
+        param_str_piece = ('_%0.3f' % param_val)
+        sweep_param_str = sweep_param_str + param_str_piece
+                
+    return sweep_param_str
