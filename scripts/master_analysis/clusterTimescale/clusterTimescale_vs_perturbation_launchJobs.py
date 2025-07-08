e148cdcec7298f7b270779fe817866ab5c640efc
Lia Papadopoulos <liap@uoregon.edu>
2025-07-07 17:39:17 -0700

    adding code: cluster timescale vs arousal mod

---
+++ scripts/master_analysis/clusterTimescale/clusterTimescale_vs_perturbation_launchJobs.py
@@ -0,0 +1,69 @@
+# LAUNCH JOBS FOR PARAMETER SWEEP
+
+
+#%% imports
+import os
+import numpy as np
+import sys
+import importlib
+
+#%% from settings
+import clusterTimescale_vs_perturbation_settings as settings
+func_path0 = settings.func_path0
+func_path = settings.func_path
+sys.path.append(func_path)
+sys.path.append(func_path0)
+
+from fcn_simulation_loading import fcn_set_sweepParam_string
+from fcn_simulation_setup import fcn_define_arousalSweep
+
+
+#%% get settings
+
+load_from_simParams = settings.load_from_simParams
+
+simParams_fname = settings.simParams_fname
+sweep_param_name = settings.sweep_param_name
+maxCores = settings.maxCores
+cores_per_job = settings.cores_per_job
+
+if load_from_simParams == True:
+    simParams_fname = settings.simParams_fname
+else:
+    n_sweepParams = settings.n_sweepParams
+    swept_params_dict = settings.swept_params_dict
+    
+
+#%% load sim parameters
+if load_from_simParams == True:
+    # load sim parameters
+    params = importlib.import_module(simParams_fname) 
+    s_params = params.sim_params
+    # arousal sweep
+    s_params = fcn_define_arousalSweep(s_params)
+    # unpack
+    n_sweepParams = s_params['nParams_sweep']
+    swept_params_dict = s_params['swept_params_dict']
+    
+
+#%% LAUNCH JOBS
+
+# tell task-spooler how many jobs it can run simultaneously
+simul_jobs = round(maxCores/cores_per_job)
+os.system("tsp -S %s" % simul_jobs)
+
+# number of parameter values
+nParam_vals = np.size(swept_params_dict['param_vals1'])
+
+# loop over swept parameter, networks and launch jobs
+for param_indx in range(0, nParam_vals):
+            
+    sweep_param_str = fcn_set_sweepParam_string(n_sweepParams, sweep_param_name, swept_params_dict, param_indx) 
+
+    
+    # COMMAND TO RUN
+    command = " tsp python clusterTimescale_vs_perturbation.py --sweep_param_str_val %s " % (sweep_param_str)
+
+    # SUBMIT JOBS
+    os.system(command) 
+
---
+++ scripts/master_analysis/clusterTimescale/clusterTimescale_vs_perturbation_settings.py
@@ -0,0 +1,83 @@
+
+"""
+settings for clusterTimescale
+"""
+
+#-------------------- basic imports ----------------------------#
+import numpy as np
+
+#-------------------- path for loading/saving data ----------------------------#
+
+load_path = ('/mnt/data0/liap/PostdocWork_Oregon/My_Projects/'\
+             'PROJ_VariabilityGainMod/data_files/test_stim_expSyn/')
+
+save_path = ('/mnt/data0/liap/PostdocWork_Oregon/My_Projects/'\
+             'PROJ_VariabilityGainMod/data_files/test_stim_expSyn/clusterTimescale/')
+    
+fig_path = ('/mnt/data0/liap/PostdocWork_Oregon/My_Projects/PROJ_VariabilityGainMod/' \
+            'data_files/Figures/test_stim_expSyn/clusterTimescale/')
+    
+func_path = '/home/liap/PostdocWork_Oregon/'\
+            'My_Projects/PROJ_VariabilityGainMod/'\
+            'scripts/functions/'
+
+func_path0 = '/home/liap/PostdocWork_Oregon/My_Projects/PROJ_VariabilityGainMod/scripts/master_sims/'
+
+sim_params_path = '/home/liap/PostdocWork_Oregon/My_Projects/PROJ_VariabilityGainMod/scripts/master_sims/'
+
+
+
+#%% load from sim params or not
+load_from_simParams = True
+
+
+#%% simulation details always specified
+#sweep_param_name = 'Jee_reduction_nu_ext_ee_beta_spread_nu_ext_ie_beta_spread'
+sweep_param_name = 'zeroMean_sd_nu_ext_ee'
+#sweep_param_name = 'same_eachClustersd_nu_ext_e_pert'
+net_type = 'baseEIclu'
+#net_type = 'baseHOM'
+nNetworks = 5
+
+#%% if loading from simParams file, give simParams_fname
+#simParams_fname = 'simParams_051325_clu_spontLong'
+simParams_fname = 'simParams_050925_clu'
+
+#%% if not loading from sim params file, need to specify different information
+
+simID = 113020232105
+nTrials = 30
+nStim = 5
+stim_shape = 'diff2exp'
+stim_type = ''
+stim_rel_amp = 0.05
+n_sweepParams = 1
+swept_params_dict = {}
+swept_params_dict['param_vals1'] = np.arange(0,0.45,0.05)
+
+
+
+#%% cluster params
+
+maxCores = 48
+cores_per_job = 4
+    
+#%% analysis params
+
+
+gain_based = True
+
+# analysis parameters [all times in seconds]
+burnTime_begin = 0.2
+burnTime_end = 0.1
+
+window_step = 1e-3
+window_std = 25e-3
+if gain_based == False:
+    rate_thresh_array = np.array([8, 10, 15, 20])
+else:
+    rate_thresh_array = np.array([0, 1])
+    
+
+rate_thresh_plot = 0
+
