d1fe54c178815c7d9b07709a963eaf20ee2ebae1
Lia Papadopoulos <liap@uoregon.edu>
2025-07-10 16:41:36 -0700

    updating paths

--- src_code/global_settings.py
+++ src_code/global_settings.py
@@ -2,3 +2,11 @@
 
 
 path_to_src_code = '/home/liap/PostdocWork_Oregon/My_Projects/PROJ_VariabilityGainMod/src_code/'
+
+path_to_sim_output = '/mnt/data0/liap/PostdocWork_Oregon/My_Projects/PROJ_VariabilityGainMod/data_files/test_stim_expSyn/'
+
+path_to_sim_figures = '/mnt/data0/liap/PostdocWork_Oregon/My_Projects/PROJ_VariabilityGainMod/data_files/Figures/test_stim_expSyn/'
+
+path_to_2clusterMFT_figures = '/mnt/data0/liap/PostdocWork_Oregon/My_Projects/PROJ_VariabilityGainMod/data_files/Figures/MFT_2Ecluster/'
+path_to_2clusterMFT_output = '/mnt/data0/liap/PostdocWork_Oregon/My_Projects/PROJ_VariabilityGainMod/data_files/MFT_2Ecluster/'
+
