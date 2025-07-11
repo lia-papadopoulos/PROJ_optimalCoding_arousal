335f84b138279e61d1c960911e4ff633a1bd93eb
Lia Papadopoulos <liap@uoregon.edu>
2025-07-11 11:09:17 -0700

    updated global settings file

--- src_code/global_settings.py
+++ src_code/global_settings.py
@@ -1,12 +1,13 @@
-# GLOBAL SETTINGS
-
+# GLOBAL PATHS
 
 path_to_src_code = '/home/liap/PostdocWork_Oregon/My_Projects/PROJ_VariabilityGainMod/src_code/'
 
 path_to_sim_output = '/mnt/data0/liap/PostdocWork_Oregon/My_Projects/PROJ_VariabilityGainMod/data_files/test_stim_expSyn/'
-
 path_to_sim_figures = '/mnt/data0/liap/PostdocWork_Oregon/My_Projects/PROJ_VariabilityGainMod/data_files/Figures/test_stim_expSyn/'
 
 path_to_2clusterMFT_figures = '/mnt/data0/liap/PostdocWork_Oregon/My_Projects/PROJ_VariabilityGainMod/data_files/Figures/MFT_2Ecluster/'
 path_to_2clusterMFT_output = '/mnt/data0/liap/PostdocWork_Oregon/My_Projects/PROJ_VariabilityGainMod/data_files/MFT_2Ecluster/'
 
+path_to_original_data = '/mnt/ion-nas2/Brain_Initiative/Neuropixels/Su_NP/ToLiaLuca/'
+path_to_processed_data = '/mnt/data0/liap/PostdocWork_Oregon/My_Projects/PROJ_VariabilityGainMod/data_files/analysis_SuData/processed_data_LP/'
+path_to_data_analysis_output = '/mnt/data0/liap/PostdocWork_Oregon/My_Projects/PROJ_VariabilityGainMod/data_files/analysis_SuData/'
