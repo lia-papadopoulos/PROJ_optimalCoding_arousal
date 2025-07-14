a445761811d9b3d346bed118dc73f398c179f7bf
Lia Papadopoulos <liap@uoregon.edu>
2025-07-14 09:31:57 -0700

    adding code: psth vs pupil in the data

---
+++ src_code/data_analysis/psth_vs_pupil/psth_vs_pupil_settings.py
@@ -0,0 +1,62 @@
+
+import sys
+
+sys.path.append('../../')
+import global_settings
+
+
+# cluster
+maxCores = 40
+cores_per_job = 4 # needs to be set ahead of time using OMP_NUM_THREADS
+
+# all sessions to run
+sessions_to_run = [\
+                   'LA3_session3', \
+                   'LA8_session1', 'LA8_session2', \
+                   'LA9_session1', \
+                   'LA9_session3', 'LA9_session4', 'LA9_session5', \
+                   'LA11_session1', 'LA11_session2', 'LA11_session3', 'LA11_session4', \
+                   'LA12_session1', 'LA12_session2', 'LA12_session3', 'LA12_session4', \
+                     
+                  ]
+    
+# paths
+data_path = global_settings.path_to_processed_data
+outpath = global_settings.path_to_data_analysis_output + 'psth_pupil/'
+func_path1 = global_settings.path_to_src_code + 'data_analysis/'      
+func_path2 = global_settings.path_to_src_code + 'functions/'   
+                
+# window for trials
+trial_window = [-200e-3, 450e-3]
+
+# baseline window
+baseline_window = [-200e-3, 0]
+
+# stimulus window
+stimulus_window = [0, 200e-3]
+
+# window length
+window_length = 100e-3
+
+# window_step
+window_step = 1e-3
+
+# stimulus duration
+stim_duration = 25e-3
+
+# size of pupil percentile bins
+pupilBlock_size = 0.1
+pupilBlock_step = 0.1
+pupilSplit_method = 'percentile'
+
+# number of trials needed
+nTrials_thresh = 20
+
+# pupil size method
+pupilSize_method = 'avgSize_beforeStim'
+
+# rest only
+restOnly = False
+trialMatch = False
+runThresh = 1.25
+runSpeed_method = 'avgSize_beforeStim'
