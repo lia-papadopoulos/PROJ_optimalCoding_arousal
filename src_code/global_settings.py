bb4aab61985b7a2e2465423cf3acb700884153ae
Lia Papadopoulos <liap@uoregon.edu>
2025-10-17 15:34:19 -0700

    updated paths

--- src_code/global_settings.py
+++ src_code/global_settings.py
@@ -1,17 +1,19 @@
-# GLOBAL PATHS
+# ABSOLUTE PATHS
+
+### these should be updated based on the user's directory structure
 
 path_to_src_code = '/home/liap/PostdocWork_Oregon/My_Projects/PROJ_VariabilityGainMod/src_code/'
-
-path_to_sim_output = '/mnt/data0/liap/PostdocWork_Oregon/My_Projects/PROJ_VariabilityGainMod/data_files/test_stim_expSyn/'
-path_to_sim_figures = '/mnt/data0/liap/PostdocWork_Oregon/My_Projects/PROJ_VariabilityGainMod/data_files/Figures/test_stim_expSyn/'
-
-path_to_2clusterMFT_figures = '/mnt/data0/liap/PostdocWork_Oregon/My_Projects/PROJ_VariabilityGainMod/data_files/Figures/MFT_2Ecluster/'
-path_to_2clusterMFT_output = '/mnt/data0/liap/PostdocWork_Oregon/My_Projects/PROJ_VariabilityGainMod/data_files/MFT_2Ecluster/'
-
-path_to_original_data = '/mnt/ion-nas2/Brain_Initiative/Neuropixels/Su_NP/ToLiaLuca/'
 path_to_processed_data = '/mnt/data0/liap/PostdocWork_Oregon/My_Projects/PROJ_VariabilityGainMod/data_files/analysis_SuData/processed_data_LP/'
+path_to_sim_output = '/mnt/data0/liap/PostdocWork_Oregon/My_Projects/PROJ_VariabilityGainMod/data_files/test_stim_expSyn/'
+path_to_2clusterMFT_output = '/mnt/data0/liap/PostdocWork_Oregon/My_Projects/PROJ_VariabilityGainMod/data_files/MFT_2Ecluster/'
 path_to_data_analysis_output = '/mnt/data0/liap/PostdocWork_Oregon/My_Projects/PROJ_VariabilityGainMod/data_files/analysis_SuData/'
-
+path_to_manuscript_figs_final = '/mnt/data0/liap/PostdocWork_Oregon/My_Projects/PROJ_VariabilityGainMod/data_files/manuscript_figs/Neuron_finalSubmission/'
 path_to_plotting_font = '/home/liap/fonts/Arial.ttf'
 
+
+### not relevant for secondary user ###
+path_to_original_data = '/mnt/ion-nas2/Brain_Initiative/Neuropixels/Su_NP/ToLiaLuca/'
+path_to_sim_figures = '/mnt/data0/liap/PostdocWork_Oregon/My_Projects/PROJ_VariabilityGainMod/data_files/Figures/test_stim_expSyn/'
+path_to_2clusterMFT_figures = '/mnt/data0/liap/PostdocWork_Oregon/My_Projects/PROJ_VariabilityGainMod/data_files/Figures/MFT_2Ecluster/'
 path_to_manuscript_figs = '/mnt/data0/liap/PostdocWork_Oregon/My_Projects/PROJ_VariabilityGainMod/data_files/manuscript_figs/Neuron_04082024/revision1/'
+path_to_manuscript_figs_rev2 = '/mnt/data0/liap/PostdocWork_Oregon/My_Projects/PROJ_VariabilityGainMod/data_files/manuscript_figs/Neuron_04082024/revision2/'
