0785f7751b6283104135bcd09335886ed00dcc02
Lia Papadopoulos <liap@uoregon.edu>
2025-10-20 14:31:06 -0700

    minor updates

--- src_code/simulations_analysis/numActive_targeted_nontargeted_clusters/numActive_targeted_nontargeted_clusters_vs_perturbation_gainBased_launchJobs.py
+++ src_code/simulations_analysis/numActive_targeted_nontargeted_clusters/numActive_targeted_nontargeted_clusters_vs_perturbation_gainBased_launchJobs.py
@@ -1,21 +1,20 @@
-# LAUNCH JOBS FOR PARAMETER SWEEP
 
-#-------------------- basic imports -------------------------------------------#
-
+#%% imports
 import os
 import numpy as np
 import sys
 import importlib
 
-import settings_file as params
+#%% settings
+import settings as params
+
+#%% functions
 func_path = params.func_path
 func_path1 = params.func_path1
-
 sys.path.append(func_path)
 sys.path.append(func_path1)
 from fcn_simulation_loading import fcn_set_sweepParam_string
 from fcn_simulation_setup import fcn_define_arousalSweep
-
 
 #%% get settings
 sim_params_path = params.sim_params_path
@@ -45,13 +44,11 @@ os.system("tsp -S %s" % simul_jobs)
 # number of parameter values
 nParam_vals = np.size(swept_params_dict['param_vals1'])
 
-# loop over swept parameter, networks and launch jobs
+# loop over arousal and launch jobs
 for param_indx in range(0, nParam_vals):
             
     sweep_param_str = fcn_set_sweepParam_string(n_sweepParams, sweep_param_name, swept_params_dict, param_indx)
   
-    # COMMAND TO RUN
-    command = "tsp python numActive_targeted_vs_nontargeted_clusters_vs_perturbation_gainBased.py --sweep_param_str_val %s " % (sweep_param_str)
+    command = "tsp python numActive_targeted_nontargeted_clusters_vs_perturbation_gainBased.py --sweep_param_str_val %s " % (sweep_param_str)
 
-    # SUBMIT JOBS
     os.system(command) 
--- src_code/simulations_analysis/numActive_targeted_nontargeted_clusters/settings.py
+++ src_code/simulations_analysis/numActive_targeted_nontargeted_clusters/settings.py
@@ -1,33 +1,30 @@
 
-"""
-settings file
-"""
-
+#%% imports
 import numpy as np
 import sys
 
 sys.path.append('../../')
 import global_settings
 
-# for task spooler
-maxCores = 48
-cores_per_job = 4 # needs to be set ahead of time using OMP_NUM_THREADS
+#%% for task spooler
+maxCores = 48 # total number of cores to use
+cores_per_job = 4 # number of cores/job; needs to be set ahead of time using OMP_NUM_THREADS
 
+#%% paths
 sim_params_path = global_settings.path_to_src_code + 'run_simulations/'
 func_path = global_settings.path_to_src_code + 'functions/'
 func_path1 = global_settings.path_to_src_code + 'run_simulations/'
 load_path = global_settings.path_to_sim_output + ''
 decoding_path = global_settings.path_to_sim_output + 'decoding_analysis/'   
 save_path = global_settings.path_to_sim_output + 'num_active_targeted_nontargeted_clusters/'
-fig_path = global_settings.path_to_sim_figures + 'num_active_targeted_nontargeted_clusters/'
 
-# simulation parameters
+#%% simulation parameters
 simParams_fname = 'simParams_051325_clu'
 sweep_param_name = 'Jee_reduction_nu_ext_ee_beta_spread_nu_ext_ie_beta_spread'
 net_type = 'baseEIclu'
 nNetworks = 10
 
-# analysis parameters
+#%% analysis parameters
 zscore = False
 preStim_burn = 200e-3
 window_length = 100e-3
@@ -37,12 +34,12 @@ gain_thresh_array = np.array([0, 1.0, 2, 3])
 rate_thresh_array = np.array([8, 12, 15, 20, 30])
 lifetimeThresh = 25e-3
 
-# decoding 
+#%% decoding params
 decode_ensembleSize = 160
 decode_windowSize = 100e-3
 decode_type = 'LinearSVC'
 decode_rateThresh = 0.
 
-# plotting
+#%% plotting
 gain_based = True
 
